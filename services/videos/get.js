5f1241799906093062064cea649b74fb13b8f2f6
Sebastian <sebasporto@gmail.com>
2013-03-29 00:20:01 +1100

    Fixed embed player for youtube

--- services/videos/get.js
+++ services/videos/get.js
@@ -1,7 +1,12 @@
 var db = require('../../db');
+var videoInfo = require('videoinfo');
 
 module.exports = {
 	run: function (id, cb) {
-		return db.get(id, cb);
+		return db.get(id, function (err, doc) {
+			// add the provider id to the video
+			doc.providerId = videoInfo.getId(doc.url);
+			return cb(null, doc);
+		});
 	}
 };
