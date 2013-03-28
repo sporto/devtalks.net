3afd1124c98ebb9001b4f9418963af0447b4f713
Sebastian <sebasporto@gmail.com>
2013-03-28 17:39:05 +1100

    Some fixes in video details retrieval

--- controllers/web/suggestions.js
+++ controllers/web/suggestions.js
@@ -5,7 +5,7 @@ module.exports = {
 	new: function (req, res) {
 
 		function done(err, tags) {
-			res.render('suggestions/new', {title: 'Express', tags: tags});
+			res.render('suggestions/new', {tags: tags});
 		}
 
 		getTagsService.run(done);
--- services/videos/get_details.js
+++ services/videos/get_details.js
@@ -2,6 +2,11 @@ var service = require('videoinfo');
 
 module.exports = {
 	run: function (url, cb) {
-		return service.fetch(url, cb);
+		return service.fetch(url, function (err, data) {
+			if (err) return cb(err);
+			console.log(data)
+			if (!data) return cb(new Error('Video not found'));
+			return cb(null, data);
+		});
 	}
 };
