3c3246bc06132e7bf6a1073a4d6ca22f76689f3b
Sebastian <sebasporto@gmail.com>
2013-03-28 15:55:57 +1100

    Redirect to origin after login

--- config/authentication.js
+++ config/authentication.js
@@ -8,7 +8,7 @@ var getUserService = require('../services/users/get');
 var findByProviderService = require('../services/users/find_by_provider');
 var createUserService = require('../services/users/create');
 
-module.exports = function (app) {
+module.exports = function(app) {
 
 	// Passport session setup.
 	//   To support persistent login sessions, Passport needs to be able to
@@ -23,7 +23,7 @@ module.exports = function (app) {
 
 	passport.deserializeUser(function(id, done) {
 		// console.log('deserializeUser');
-		getUserService.run(id, function (err, user) {
+		getUserService.run(id, function(err, user) {
 			done(err, user);
 		});
 	});
@@ -33,66 +33,60 @@ module.exports = function (app) {
 	//   credentials (in this case, an accessToken, refreshToken, and GitHub
 	//   profile), and invoke a callback with a user object.
 	passport.use(new GitHubStrategy({
-			clientID: GITHUB_CLIENT_ID,
-			clientSecret: GITHUB_CLIENT_SECRET,
-			callbackURL: "http://127.0.0.1:3000/auth/github/callback"
-	  },
-		function(accessToken, refreshToken, profile, done) {
-			// console.log(profile);
+		clientID: GITHUB_CLIENT_ID,
+		clientSecret: GITHUB_CLIENT_SECRET,
+		callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+	}, function(accessToken, refreshToken, profile, done) {
+		// console.log(profile);
+		findByProviderService.run('github', profile.id, function(err, user) {
+			// console.log(err);
+			// console.log(user);
+			if(user) {
+				return done(err, user);
+			}
 
-			findByProviderService.run('github', profile.id, function (err, user) {
-	    	// console.log(err);
-	    	// console.log(user);
-	    	if (user) {
-	    		return done(err, user);
-	    	}
+			var userInfo = {
+				username: profile.username,
+				provider: 'github',
+				providerId: profile.id,
+				avatarUrl: profile._json.avatar_url,
+				gravatarId: profile._json.gravatar_id
+			}
 
-	    	var userInfo = {
-	    		username: profile.username,
-	    		provider: 'github',
-	    		providerId: profile.id,
-	    		avatarUrl: profile._json.avatar_url,
-	    		gravatarId: profile._json.gravatar_id
-	    	}
+			createUserService.run(userInfo, function(err, user) {
+				return done(err, user);
+			});
 
-	    	createUserService.run(userInfo, function (err, user) {
-					return done(err, user);
-	    	});
-
-	    });
-	  }
-	));
+		});
+	}));
 
 	// GET /auth/github
 	//   Use passport.authenticate() as route middleware to authenticate the
 	//   request.  The first step in GitHub authentication will involve redirecting
 	//   the user to github.com.  After authorization, GitHubwill redirect the user
 	//   back to this application at /auth/github/callback
-	app.get('/auth/github',
-		passport.authenticate('github'),
-		function(req, res){
-		  // The request will be redirected to GitHub for authentication, so this
-		  // function will not be called.
-		});
+	app.get('/auth/github', passport.authenticate('github'), function(req, res) {
+		// The request will be redirected to GitHub for authentication, so this
+		// function will not be called.
+	});
 
 	// GET /auth/github/callback
 	//   Use passport.authenticate() as route middleware to authenticate the
 	//   request.  If authentication fails, the user will be redirected back to the
 	//   login page.  Otherwise, the primary route function function will be called,
 	//   which, in this example, will redirect the user to the home page.
-	app.get('/auth/github/callback', 
-		passport.authenticate('github', { failureRedirect: '/login' }),
-		function(req, res) {
-			res.redirect('/');
-		});
+	app.get('/auth/github/callback', passport.authenticate('github', {
+		failureRedirect: '/login'
+	}), function(req, res) {
+		res.redirect('back')
+	});
 
 	// app.get('/login', function(req, res){
 	// 	res.render('sessions/new', { user: req.user });
 	// });
-
-	app.get('/logout', function(req, res){
-	  req.logout();
-	  res.redirect('/');
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
 	});
 
 }
