9fbcafb08998245c24c76134da033e8d6c119808
Sebastian <sebasporto@gmail.com>
2013-03-31 21:34:19 +1100

    Using the router fully in video/index

--- assets/js/app/modules/videos/index.js
+++ assets/js/app/modules/videos/index.js
@@ -9,14 +9,16 @@ angular.module('APP')
 			// the $route needs to be injected so angular will trigger the route events
 
 		$scope.videos = [];
-		$scope.tags = $element.data('tags');
+		$scope.allTags = $element.data('tags');
+		$scope.query = {tags: []};
 
-		$scope.clickTag = function (ix, tag) {
-			tag.selected = !tag.selected;
-			setRoute();
+		$scope.addTag = function (ix, tag) {
+			addTagToRoute(ix, tag.key);
 		}
-		// TODO get the selected tags in the UI from the router
-		// So when going back they shows as selected
+
+		$scope.removeTag = function (ix, tag) {
+			removeTagFromRoute(ix, tag);
+		}
 
 		$scope.selected = function (item) {
 			return item.selected;
@@ -26,28 +28,61 @@ angular.module('APP')
 			return !item.selected;
 		}
 
-		function getSelectedTags() {
-			var tags = [];
-			_.each($scope.tags, function (ele) {
-				if (ele.selected) tags.push(ele.key);
-			});
-			return tags;
-		}
-
-		function setRoute() {
-			$location.url('/search');
-			$location.search({tags: getSelectedTags()});
-		}
-
 		$scope.$on('$routeChangeSuccess', function (scope, next, current) {
-			search();
+			processRoute();
 		});
 
-		function search() {
-			// get query from hash
+		// at the moment angular doesn't handle arrays in routes at it should
+		function getSelectedTagsFromRoute() {
 			var params = $location.search();
+			var tags = params.tags || [];
+			if (_.isArray(tags)) {
+				return tags;
+			} else {
+				return tags.split(',');
+			}
+		}
+
+		function addTagToRoute(ix, tag) {
+			$location.path('/search');
+			var tags = getSelectedTagsFromRoute();
+			// do no push if already there
+			if (_.contains(tags, tag)) return;
+			tags.push(tag);
+			$location.search({tags: tags});
+		}
+
+		function removeTagFromRoute(ix, tag) {
+			var tags = getSelectedTagsFromRoute();
+			tags.splice(ix, 1);
+			$location.search({tags: tags});
+		}
+
+		function getQueryFromRoute() {
+			var tags = getSelectedTagsFromRoute();
+			return {tags: tags};
+		}
+
+		function processRoute() {
+			refreshQuery();
+			search();
+		}
+
+		function refreshQuery() {
+			$scope.query = getQueryFromRoute();
+			// refreh the list of tags in the ui
+			_.each($scope.allTags, function (ele) {
+				if (_.contains($scope.query.tags, ele.key)) {
+					ele.selected = true;
+				} else {
+					ele.selected = false;
+				}
+			});
+		}
+
+		function search() {
 			var config = {
-				params: params
+				params: $scope.query
 			}
 			$http.get('/api/v1/videos/search', config)
 			.success(function (data, status, headers, config) {
