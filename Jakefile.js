51f69449c4eeb52e2aa55f7c2829840dfe6016f9
Sebastian <sebasporto@gmail.com>
2013-04-01 23:21:27 +1100

    DB in jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,18 @@
+var nconf = require('nconf');
+
 desc('This is the default task.');
 task('views', {async: true}, function (params) {
 
-	var couchpenter = new (require('couchpenter'))(
-		'http://localhost:5984/'
-	);
+	nconf.use('memory').env().file({file: './env.json'});
+
+	var dbPath = "";
+	if (process.env.NODE_ENV == 'production') {
+		dbPath = nconf.get('DB_PATH_PRO');
+	} else {
+		dbPath = nconf.get('DB_PATH_DEV');
+	}
+
+	var couchpenter = new (require('couchpenter'))(dbPath);
 
 	couchpenter.setUpDocumentsOverwrite(function (err, results) {
 		console.log(err);
