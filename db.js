1c891c7080a3b89a51180793e7c39e716844421e
Sebastian <sebasporto@gmail.com>
2013-04-02 17:21:47 +1100

    Added DB name to env vars

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 var nconf = require('nconf');
 var dbPath = "";
+var dbName = "";
 
 if (process.env.NODE_ENV == 'production') {
 	dbPath = nconf.get('DB_PATH_PRO');
+	dbName = nconf.get('DB_NAME_PRO');
 } else {
 	dbPath = nconf.get('DB_PATH_DEV');
+	dbName = nconf.get('DB_NAME_DEV');
 }
+
 var nano = require('nano')(dbPath);
-var videos = nano.db.use('videos')
+var videos = nano.db.use(dbName);
 
 module.exports = videos;
 
