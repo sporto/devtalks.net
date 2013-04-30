164de1c22854795d206360795220c76ea44f6805
Sebastian <sebasporto@gmail.com>
2013-04-30 22:23:15 +1000

    Some test clean up

--- test/services/videos/get_details_spec.js
+++ test/services/videos/get_details_spec.js
@@ -1,52 +1,29 @@
-var assert = require("assert");
-var rewire = require("rewire");
-var sinon = require("sinon");
-var subject = require('../../../services/videos/get_details');
+var assert =    require("assert");
+var rewire =    require("rewire");
+var sinon =     require("sinon");
+var subject =   rewire('../../../services/videos/get_details');
 
-var fakeYoutube = {
-	run: function (id, cb) {
-		return cb(null, {provider: 'youtube'});
+var url = "http://vimeo.com/27246366";
+
+var fakeService = {
+	fetch: function (url, cb) {
+		cb(null, {});
 	}
 }
-subject._youtube = fakeYoutube;
+
+subject.__set__("service", fakeService);
 
 describe('get_details', function() {
 
-	describe('unknown', function () {
-		var url = "http://www.xxexxx.com/watch?v=wxtFtVfAeeE";
+	it('asks the service', function (done) {
+		sinon.spy(fakeService, "fetch");
 
-		it('responds with err', function () {
-			subject.run(url, function (err, info) {
-				assert(err);
-			});
+		subject.run(url, function (err, info) {
+			assert(fakeService.fetch.calledOnce);
+			done();
+			fakeService.fetch.restore(); // Unwraps the spy
 		});
+		
 	});
-
-	describe('youtube', function() {
-
-		var url = "http://www.youtube.com/watch?v=wxtFtVfAeeE";
-
-		it('it should know it is youtube', function(done) {
-			subject.run(url, function (err, info) {
-				assert.equal(info.provider, 'youtube');
-				done();
-			});
-		});
-	});
-
-	// describe('vimeo', function() {
-	// 	var url = "http://vimeo.com/27246366";
-
-	// 	it('it should know it is youtube', function() {
-	// 		var res = subject.run(url);
-	// 		assert.equal(res.provider, 'vimeo')
-	// 	})
-
-
-	// 	it('it find the video id', function() {
-	// 		var res = subject.run(url);
-	// 		assert.equal(res.id, '27246366')
-	// 	})
-	// });
 
 })
