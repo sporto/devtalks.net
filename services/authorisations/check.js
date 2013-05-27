df9f6a0bdebb11e256942d994dabe2a223075fbb
Sebastian <sebasporto@gmail.com>
2013-05-27 21:49:23 +1000

    test

--- services/authorisations/check.js
+++ services/authorisations/check.js
@@ -2,13 +2,16 @@ var _               = require('underscore');
 var logger          = require('../../logger');
 var nconf           = require('nconf');
 var ADMIN_USERS     = nconf.get('ADMIN_USERS');
-var adminUsers      = ADMIN_USERS.split(',');
+
+function getAdminUsers() {
+	return ADMIN_USERS.split(',');
+}
 
 module.exports = {
 	run: function (user, resource, action, cb) {
 		if (user) {
 			if (resource === 'video' && action === 'manage') {
-				if (_.contains(adminUsers, user.username)) {
+				if (_.contains(getAdminUsers(), user.username)) {
 					return cb(null, true);
 				}
 			}
---
+++ test/services/authorisations/check_spec.js
@@ -0,0 +1,37 @@
+var assert =    require("assert");
+var rewire =    require("rewire");
+var serv =      rewire('../../../services/authorisations/check');
+
+serv.__set__("ADMIN_USERS", 'sam');
+
+var user = {
+	username: 'sam'
+}
+var resource = 'video';
+var action = 'manage';
+
+describe('check', function() {
+
+	it('is true', function (done) {
+		serv.run(user, resource, action, function (err, res) {
+			assert(res);
+			done();
+		});
+	});
+
+	it('is false for a not admin', function (done) {
+		user.username = 'xyz';
+		serv.run(user, resource, action, function (err, res) {
+			assert(!res);
+			done();
+		});
+	});
+
+	it('is false for a dif action', function (done) {
+		serv.run(user, resource, 'update', function (err, res) {
+			assert(!res);
+			done();
+		});
+	});
+
+})
