3f130ba176dc81e4de4403de301b188216e92ba0
Sebastian <sebasporto@gmail.com>
2013-06-21 20:14:19 +1000

    Show spinner when approving or deleting a video

--- assets/js/app/modules/admin/suggestions/index.js
+++ assets/js/app/modules/admin/suggestions/index.js
@@ -7,22 +7,29 @@ angular.module('APP')
 			$scope.videos = _.map($scope.seed_videos, function (data) { return new Video(data) });
 
 			$scope.clickApprove = function (ix, video) {
-
+				video.busy = true;
 				video.$approve(function (doc) {
 					notifyUserService.flashSuccess('Approved');
 					$scope.videos.splice(ix, 1);
 				}, function (res) {
 					notifyUserService.flashError(res.data);
+				})
+				.always(function () {
+					video.busy = true;
 				});
 
 			}
 
 			$scope.clickDelete = function (ix, video) {
 				if (confirm("Are you sure?")) {
+					video.busy = true;
 					video.$delete(function () {
 						$scope.videos.splice(ix, 1);
 					}, function (res) {
 						notifyUserService.flashError(res.data);
+					})
+					.always(function () {
+						video.busy= false;
 					});
 				}
 			}
--- test/services/authorisations/check_spec.js
+++ test/services/authorisations/check_spec.js
@@ -34,4 +34,4 @@ describe('check', function() {
 		});
 	});
 
-})
+});
